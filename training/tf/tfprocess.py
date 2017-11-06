9e6a7929806422f2f1d5e32b493ffc49405dcd41
Gian-Carlo Pascutto <gcp@sjeng.org>
2017-11-06 13:24:02 +0100

    Get the Batch Normalization weights.
    Batch Normalization layers in TensorFlow are a complete mess (but you
    have many messes to choose from!). Use a trick to key the scopes with a
    random string and get them back that way when writing out the weight
    files.

--- training/tf/tfprocess.py
+++ training/tf/tfprocess.py
@@ -2,6 +2,7 @@
 
 import os
 import numpy as np
+import uuid
 import tensorflow as tf
 
 def weight_variable(shape):
@@ -17,22 +18,6 @@ def bias_variable(shape):
 # out by the BatchNorm layers's mean adjustment.
 def bn_bias_variable(shape):
     initial = tf.constant(0.0, shape=shape)
-    return tf.Variable(initial, trainable=False)
-
-# Offsets after BN are at least not immediately cancelled out,
-# but empirically they do not help much if anything.
-# By default, without offset, about half the ReLU units after a BN fire,
-# and this is pretty optimal. Leela doesn't incorporate these right now.
-# They could be added if they turn out to be useful anyway.
-def offset_variable(shape):
-    initial = tf.constant(0.0, shape=shape)
-    return tf.Variable(initial, trainable=False)
-
-# The ReLU non-linearity is actually linear wrt scaling factors,
-# so any scaling here could be absorbed into the next layers'
-# convolution weights. No point in learning a scale factor.
-def scale_variable(shape):
-    initial = tf.constant(1.0, shape=shape)
     return tf.Variable(initial, trainable=False)
 
 def conv2d(x, W):
@@ -53,33 +38,35 @@ class TFProcess:
         self.x = tf.placeholder(tf.float32, [None, 18, 19 * 19])
         self.y_ = tf.placeholder(tf.float32, [None, 362])
         self.z_ = tf.placeholder(tf.float32, [None, 1])
+        self.training = tf.placeholder(tf.bool)
         self.y_conv, self.z_conv = self.construct_net(self.x)
 
-        self.cross_entropy = \
+        cross_entropy = \
             tf.nn.softmax_cross_entropy_with_logits(labels=self.y_,
                                                     logits=self.y_conv)
-        self.policy_loss = tf.reduce_mean(self.cross_entropy)
+        self.policy_loss = tf.reduce_mean(cross_entropy)
         tf.summary.scalar('policy_loss', self.policy_loss)
 
         self.mse_loss = \
             tf.reduce_mean(tf.squared_difference(self.z_, self.z_conv))
         tf.summary.scalar('mse_loss', self.mse_loss)
 
-        self.loss = 1.0 * self.policy_loss + 1.0 * self.mse_loss
+        loss = 1.0 * self.policy_loss + 1.0 * self.mse_loss
 
-        self.train_step = \
-            tf.train.MomentumOptimizer(
-                learning_rate=0.05, momentum=0.9, use_nesterov=True).\
-                minimize(self.loss, global_step=self.global_step)
+        opt_op = tf.train.MomentumOptimizer(
+            learning_rate=0.05, momentum=0.9, use_nesterov=True)
 
-        self.correct_prediction = \
+        self.update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(self.update_ops):
+            self.train_op = \
+                opt_op.minimize(loss, global_step=self.global_step)
+
+        correct_prediction = \
             tf.equal(tf.argmax(self.y_conv, 1), tf.argmax(self.y_, 1))
-        self.correct_prediction = \
-            tf.cast(self.correct_prediction, tf.float32)
-        self.accuracy = tf.reduce_mean(self.correct_prediction)
+        correct_prediction = tf.cast(correct_prediction, tf.float32)
+        self.accuracy = tf.reduce_mean(correct_prediction)
         tf.summary.scalar('accuracy', self.accuracy)
 
-        # Running averages
         self.avg_policy_loss = 8.0
         self.avg_mse_loss = 2.0
 
@@ -95,10 +82,11 @@ class TFProcess:
     def process(self, batch):
         # Run training for this batch
         _, policy_loss, mse_loss = self.session.run(
-            [self.train_step, self.policy_loss, self.mse_loss],
+            [self.train_op, self.policy_loss, self.mse_loss],
             feed_dict={self.x: batch[0],
                        self.y_: batch[1],
-                       self.z_: batch[2]})
+                       self.z_: batch[2],
+                       self.training: True})
         steps = tf.train.global_step(self.session, self.global_step)
         # Keep running averages
         # XXX: use built-in support like tf.moving_average_variables
@@ -113,12 +101,14 @@ class TFProcess:
                 self.accuracy.eval(session=self.session,
                                    feed_dict={self.x: batch[0],
                                               self.y_: batch[1],
-                                              self.z_: batch[2]})
+                                              self.z_: batch[2],
+                                              self.training: False})
             train_mse = \
                 self.mse_loss.eval(session=self.session,
                                    feed_dict={self.x: batch[0],
                                               self.y_: batch[1],
-                                              self.z_: batch[2]})
+                                              self.z_: batch[2],
+                                              self.training: False})
             print("step {0}, training accuracy={1}%, mse={2}".format(
                 steps, train_accuracy*100.0, train_mse))
             path = os.path.join(os.getcwd(), "leelaz-model")
@@ -131,37 +121,49 @@ class TFProcess:
     def save_leelaz_weights(self, filename):
         with open(filename, "w") as file:
             # Version tag
-            file.write("1\n")
+            file.write("1")
             for weights in self.weights:
-                # TF
-                # [filter_height, filter_width, in_channels, out_channels]
-                # Leela
-                # (output, input, filter_size, filter_size]
-                # Fully connected layers are [in, out] in both
+                # Newline unless last line (single bias)
+                file.write("\n")
                 work_weights = None
-                if weights.shape.ndims == 4:
+                # Keyed batchnorm weights
+                if isinstance(weights, str):
+                    work_weights = tf.get_default_graph().get_tensor_by_name(weights)
+                elif weights.shape.ndims == 4:
+                    # Convolution weights need a transpose
+                    # TF
+                    # [filter_height, filter_width, in_channels, out_channels]
+                    # Leela
+                    # [output, input, filter_size, filter_size]
                     work_weights = tf.transpose(weights, [3, 2, 0, 1])
                 else:
+                    # Fully connected layers are [in, out] in both
+                    # As are biases etc
                     work_weights = weights
                 nparray = work_weights.eval(session=self.session)
-                wt_str = (str(wt) for wt in np.ravel(nparray))
+                wt_str = [str(wt) for wt in np.ravel(nparray)]
                 file.write(" ".join(wt_str))
-                # Newline unless last line (single bias)
-                if len(wt_str) > 1:
-                    file.write("\n")
 
     def conv_block(self, inputs, input_channels, output_channels):
         W_conv = weight_variable([3, 3, input_channels, output_channels])
         b_conv = bn_bias_variable([output_channels])
-        s_conv = scale_variable([output_channels])
-        o_conv = offset_variable([output_channels])
         self.weights.append(W_conv)
         self.weights.append(b_conv)
+        # The weights are internal to the batchnorm layer, so apply
+        # a unique scope that we can store, and use to look them back up
+        # later on.
+        weight_key = uuid.uuid4().hex
+        self.weights.append(weight_key + "/batch_normalization/moving_mean:0")
+        self.weights.append(weight_key + "/batch_normalization/moving_variance:0")
 
-        h_bn, bm, vn = \
-            tf.nn.fused_batch_norm(tf.nn.bias_add(conv2d(inputs, W_conv),
-                                                  b_conv, data_format='NCHW'),
-                                   s_conv, o_conv, None, None, data_format='NCHW')
+        with tf.variable_scope(weight_key):
+            h_bn = \
+                tf.layers.batch_normalization(
+                    tf.nn.bias_add(conv2d(inputs, W_conv),
+                                          b_conv, data_format='NCHW'),
+                                   epsilon=1e-5, axis=1, fused=True,
+                                   center=False, scale=False,
+                                   training=self.training)
         h_conv = tf.nn.relu(h_bn)
         return h_conv
 
@@ -169,28 +171,38 @@ class TFProcess:
         # First convnet
         W_conv_1 = weight_variable([3, 3, channels, channels])
         b_conv_1 = bn_bias_variable([channels])
-        s_conv_1 = scale_variable([channels])
-        o_conv_1 = offset_variable([channels])
         self.weights.append(W_conv_1)
         self.weights.append(b_conv_1)
+        weight_key_1 = uuid.uuid4().hex
+        self.weights.append(weight_key_1 + "/batch_normalization/moving_mean:0")
+        self.weights.append(weight_key_1 + "/batch_normalization/moving_variance:0")
 
         # Second convnet
         W_conv_2 = weight_variable([3, 3, channels, channels])
         b_conv_2 = bn_bias_variable([channels])
-        s_conv_2 = scale_variable([channels])
-        o_conv_2 = offset_variable([channels])
         self.weights.append(W_conv_2)
         self.weights.append(b_conv_2)
+        weight_key_2 = uuid.uuid4().hex
+        self.weights.append(weight_key_2 + "/batch_normalization/moving_mean:0")
+        self.weights.append(weight_key_2 + "/batch_normalization/moving_variance:0")
 
-        h_bn1, bm1, vn1 = \
-            tf.nn.fused_batch_norm(tf.nn.bias_add(conv2d(inputs, W_conv_1),
-                                                  b_conv_1, data_format='NCHW'),
-                                   s_conv_1, o_conv_1, None, None, data_format='NCHW')
+        with tf.variable_scope(weight_key_1):
+            h_bn1 = \
+                tf.layers.batch_normalization(
+                    tf.nn.bias_add(conv2d(inputs, W_conv_1),
+                                   b_conv_1, data_format='NCHW'),
+                                   epsilon=1e-5, axis=1, fused=True,
+                                   center=False, scale=False,
+                                   training=self.training)
         h_out_1 = tf.nn.relu(h_bn1)
-        h_bn2, bm2, vn2 = \
-            tf.nn.fused_batch_norm(tf.nn.bias_add(conv2d(h_out_1, W_conv_2),
-                                                  b_conv_2, data_format='NCHW'),
-                                   s_conv_2, o_conv_2, None, None, data_format='NCHW')
+        with tf.variable_scope(weight_key_2):
+            h_bn2 = \
+                tf.layers.batch_normalization(
+                    tf.nn.bias_add(conv2d(h_out_1, W_conv_2),
+                                          b_conv_2, data_format='NCHW'),
+                                   epsilon=1e-5, axis=1, fused=True,
+                                   center=False, scale=False,
+                                   training=self.training)
         h_out_2 = tf.nn.relu(h_bn2 + inputs)
         return h_out_2
 
