c06b9cb723007e73c462dea50372a0800a1f938b
Gian-Carlo Pascutto <gcp@sjeng.org>
2017-11-08 13:21:54 +0100

    Add license to TensorFlow sources.
    Make it clearer which license applies to the TensorFlow Python code.

--- training/tf/tfprocess.py
+++ training/tf/tfprocess.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+#
+#    This file is part of Leela Zero.
+#    Copyright (C) 2017 Gian-Carlo Pascutto
+#
+#    Leela Zero is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Leela Zero is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Leela Zero.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import numpy as np
