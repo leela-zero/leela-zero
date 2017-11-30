9c92c99c6ea2e0ca01a75add1fec28f4bd799e58
Gian-Carlo Pascutto <gcp@sjeng.org>
2017-11-30 11:18:45 +0100

    Clean up KoState and hash handling.
    Cleanups begat further cleanups. Remove the worst style inconsistencies
    in the KoState class and related hash and ko hash handling.

--- src/KoState.h
+++ src/KoState.h
@@ -27,8 +27,8 @@
 class KoState : public FastState {
 public:
     void init_game(int size, float komi);
-    bool superko(void);
-    bool superko(uint64 newhash);
+    bool superko(void) const;
+    bool superko(uint64 newhash) const;
     void reset_game();
 
     void play_pass(void);
@@ -36,8 +36,8 @@ public:
     void play_move(int vertex);
 
 private:
-    std::vector<uint64> ko_hash_history;
-    std::vector<uint64> hash_history;
+    std::vector<uint64> m_ko_hash_history;
+    std::vector<uint64> m_hash_history;
 };
 
 #endif
