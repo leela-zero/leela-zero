d7b8e2e79c151077387752074210071dd2eaa450
Andy Olsen <andy.olsen.tx@gmail.com>
2017-12-17 15:36:50 -0600

    Refactor, including ideas from @sethtroisi

--- scripts/training_analysis/training_analysis.py
+++ scripts/training_analysis/training_analysis.py
@@ -22,48 +22,47 @@ import math
 import fileinput
 import collections
 import os
+import itertools
 
-BOARDSIZE = 19
 NIBBLE = 4
+BOARDSIZE = 19
+TURNSIZE = 19
 HISTORY_PLANES = 8
+TOMOVE = 16
+POLICY = 17
+WINNER = 18
+ZERO_LINE = "0"*math.ceil((BOARDSIZE**2+1)/NIBBLE) + "\n" # 361+1 bits in hex nibbles
 
-def hook_compressed_encoded(encoding):
-    def hook_compressed(filename, mode):
-        ext = os.path.splitext(filename)[1]
-        if ext == '.gz':
-            #return gzip.open(filename, mode, encoding=encoding) # this doesn't work, why?
-            return gzip.open(filename, "rt", encoding=encoding)
-        else:
-            return open(filename, mode, encoding=encoding)
-    return hook_compressed
-
-def findEmptyBoard(tfh, hist):
-    first_move_cnt = 0
-    zeros = "0"*math.ceil((BOARDSIZE**2+1)/NIBBLE) + "\n" # 362 bits in hex nibbles
+def findEmptyBoard(tfh):
+    count = collections.Counter()
     while (1):
         empty_board = True
-        for _ in range(HISTORY_PLANES*2):
-            line = tfh.readline()
-            if not line: break
-            empty_board = empty_board and line == zeros
-        if not line: break
-        to_move = int(tfh.readline())           # 0 = black, 1 = white
-        policy_weights = tfh.readline()         # 361 moves + 1 pass
-        side_to_move_won = int(tfh.readline())  # 1 = side to move won, -1 = lost
+        turn = [line.decode("utf-8") for line in itertools.islice(tfh, TURNSIZE)]
+        if len(turn) < TURNSIZE:
+            break
+        board = turn[:HISTORY_PLANES*2]
+        to_move = int(turn[TOMOVE])              # 0 = black, 1 = white
+        policy_weights = turn[POLICY].split()    # 361 moves + 1 pass
+        side_to_move_won = int(turn[WINNER])     # 1 = side to move won, -1 = lost
+        empty_board = all(line == ZERO_LINE for line in board)
         # Note: it can be empty_board and white to move if black passed
         if empty_board and to_move == 0:
-            first_move_cnt += 1
-            policy_weights = policy_weights.split()
-            cnt = policy_weights.count("0")
-            hist[cnt] += 1
-    print(dict(hist))
-    return first_move_cnt
+            #pass_weight = float(policy_weights[-1])
+            #print("pass weight %0.2f" % (pass_weight*1000))
+            count[policy_weights.count("0")] += 1
+    return count
 
-if __name__ == "__main__":
+def main():
     usage_str = """
+Update: This method will not work on newer nets, becauase the
+newer nets are starting to narrow the search. So even a normally
+working system will have many zeros in the policy target for the
+first move. This was originally developed against net 2184b750,
+where this method did work.
+
 This script analyzes the training data for abnormal results,
 such as issues #359/#375. It looks for the first move of a game
-and does a histogram of how many policy training targets have 
+and does a histogram of how many policy training targets have
 a value of "0". Normally there should be zero but with the bug
 there can be many. Note the bug is intermittent so there will
 be more games with bad data in them that this script can detect.
@@ -83,11 +82,16 @@ etc.
     parser.add_argument("files", metavar="files", type=str, nargs="+", help="training files (with or without *.gz)")
     args = parser.parse_args()
     total_first_move_cnt = 0
-    hist = collections.defaultdict(int)
+    totalCount = collections.Counter()
     for filename in args.files:
-        tfh = fileinput.FileInput(filename, openhook=hook_compressed_encoded("utf-8"))
-        print(filename, " ", end="")
-        total_first_move_cnt += findEmptyBoard(tfh, hist)
-    print("total first move cnt = ", total_first_move_cnt)
+        tfh = fileinput.FileInput(filename, openhook=fileinput.hook_compressed)
+        fileCount = findEmptyBoard(tfh)
+        print(filename, fileCount)
+        totalCount.update(fileCount)
+    print("first move counts =", totalCount)
+    print("total first move counts =", sum(totalCount.values()))
+
+if __name__ == "__main__":
+    main()
 
 
