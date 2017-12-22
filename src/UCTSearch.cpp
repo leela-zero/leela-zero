de61a73a420dd565a034203a4b9479a12f2e28db
michael <Michael O>
2017-12-23 08:36:59 +1100

    Cache the network evaluation results.
    Network::get_scored_moves() is often called with duplicate inputs due to both
    search tree convergence, and portions of the search tree being in common between
    moves.
    
    1. Add a cache for the Network::Netresults
    2. Remove TTable as it's no longer needed.
    3. Print out cache stats on each turn.
    
    Playing a game (-t 1 -s 1 -p 1600 -m 30 -n -d  --noponder) after this change
    was about 2x faster for most of the game, speeding up to ~4x near the end
    as the search tree narrows.

--- src/UCTSearch.cpp
+++ src/UCTSearch.cpp
@@ -47,12 +47,9 @@ UCTSearch::UCTSearch(GameState & g)
 
 SearchResult UCTSearch::play_simulation(GameState & currstate, UCTNode* const node) {
     const auto color = currstate.get_to_move();
-    const auto hash = currstate.board.get_hash();
-    const auto komi = currstate.get_komi();
 
     auto result = SearchResult{};
 
-    TTable::get_TT()->sync(hash, komi, node);
     node->virtual_loss();
 
     if (!node->has_children()) {
@@ -96,7 +93,6 @@ SearchResult UCTSearch::play_simulation(GameState & currstate, UCTNode* const no
         node->update(result.eval());
     }
     node->virtual_loss_undo();
-    TTable::get_TT()->update(hash, komi, node);
 
     return result;
 }
