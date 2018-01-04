93b87851836cc03b1e74b66f1fb1e5494fc71866
Marco Calignano <marco.calignano@gmail.com>
2018-01-04 12:10:38 +0100

    autogtp: resore json output

--- autogtp/Management.cpp
+++ autogtp/Management.cpp
@@ -213,6 +213,8 @@ Order Management::getWorkInternal() {
         throw NetworkException("JSON parse error: " + errorString);
     }
 
+    QTextStream(stdout) << doc.toJson() << endl;
+    
     QJsonObject ob = doc.object();
     QJsonObject opt = ob.value("options").toObject();
     QString options;
