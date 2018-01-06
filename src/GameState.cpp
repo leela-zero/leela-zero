6ef4de2400619f75013b192b69bfd739ee44f3a5
Andy Olsen <andy.olsen.tx@gmail.com>
2018-01-06 13:20:52 -0600

    Fix assert in GameState.
    Pull request #575.

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -336,6 +336,6 @@ void GameState::place_free_handicap(int stones) {
 
 const FullBoard& GameState::get_past_board(int moves_ago) const {
     assert(moves_ago >= 0 && (unsigned)moves_ago <= m_movenum);
-    assert(m_movenum + 1 == game_history.size());
+    assert(m_movenum + 1 <= game_history.size());
     return game_history[m_movenum - moves_ago]->board;
 }
