b17f6952062b95cfdd2568b666a7d0cf170da330
Henrik Forstén <henrik.forsten@gmail.com>
2018-01-23 16:24:36 +0200

    Unequal input weighting. Widen the first convolution in block randomly

--- training/tf/net2net.py
+++ training/tf/net2net.py
@@ -63,7 +63,7 @@ def read_net(filename):
 
 def conv_bn_wider(weights, next_weights, inputs, channels,
                   new_channels, noise_std=0, last_block=False,
-                  rand=None, verify=False):
+                  rand=None, dir_alpha=None, verify=False):
 
     if new_channels == 0:
         return weights, next_weights
@@ -72,6 +72,24 @@ def conv_bn_wider(weights, next_weights, inputs, channels,
         rand = list(range(channels))
         rand.extend(np.random.randint(0, channels, new_channels))
     rep_factor = np.bincount(rand)
+
+    factor = np.zeros(len(rand))
+
+    #In the net2net paper every input weight was weighted equally,
+    #but in general we can have unequal division of the weights
+    if dir_alpha == None:
+        #Equal division
+        for i in range(len(rand)):
+            factor[i] = 1.0/rep_factor[rand[i]]
+    else:
+        #Unequal input weighting determined by dirichlet distribution
+        for i in range(channels):
+            x = np.random.dirichlet([dir_alpha]*rep_factor[i])
+            e = 0
+            for j in range(channels + new_channels):
+                if rand[j] == i:
+                    factor[j] = x[e]
+                    e += 1
 
     #Widen the current layer
     w_conv_new = np.array(weights[0]).reshape(channels, inputs, 3, 3)[rand, :, :, :]
@@ -93,7 +111,7 @@ def conv_bn_wider(weights, next_weights, inputs, channels,
             noise = 0
             if i >= channels:
                 noise = np.random.normal(0, noise_std)
-            next_weights_new[j][:, i, :, :] *= (1.0 + noise)/rep_factor[rand[i]]
+            next_weights_new[j][:, i, :, :] *= (1.0 + noise)*factor[i]
 
     if noise_std == 0 and verify:
         x = np.random.random((inputs, 19, 19))
@@ -124,15 +142,28 @@ if __name__ == "__main__":
     parser.add_argument("blocks", help="Residual blocks to add", type=int)
     parser.add_argument("filters", help="Filters to add", type=int)
     parser.add_argument("network", help="Input network", type=str)
-    parser.add_argument("--noise", nargs='?', help="Standard deviation of noise to add to new filters/blocks. Default: 5e-3", default=5e-3, type=float)
-    parser.add_argument("--verify", help="Verify that output matches", default=False, action='store_true')
+    parser.add_argument("--noise", nargs='?', help="Standard deviation of noise to add to new filters/blocks. Default: 5e-3",
+            default=5e-3, type=float)
+    parser.add_argument("--dir_alpha", nargs='?', help=\
+        """Dirichlet distribution parameter for input weight distribution for replicated channels. """\
+        """Larger values divide input values more equally. """\
+        """Smaller ones give one large input weight while others are very small. """\
+        """You probably want this to be at least 1 to avoid near zero weights. """\
+        """Set to 0 to divide input weights equally. Default: 10""",
+        default=10, type=float)
+    parser.add_argument("--verify", help="Verify that output matches. Noise must be disabled.",
+            default=False, action='store_true')
 
     args = parser.parse_args()
     new_blocks = args.blocks
     new_channels = args.filters
     net_filename = args.network
     noise_std = args.noise
+    dir_alpha = args.dir_alpha
     verify = args.verify
+
+    if dir_alpha <= 0:
+        dir_alpha = None
 
     if verify and noise_std != 0:
         raise ValueError("Noise must be zero if verify is enabled.")
@@ -146,6 +177,9 @@ if __name__ == "__main__":
 
     if new_channels < 0:
         raise ValueError("Filters must be non-negative")
+
+    print("Output will have {} blocks and {} channels.".format(
+        blocks+new_blocks, channels+new_channels))
 
     input_planes = 18
 
@@ -186,20 +220,27 @@ if __name__ == "__main__":
     rand.extend(np.random.randint(0, channels, new_channels))
 
     #Input
-    w_wider, conv_next = conv_bn_wider(w_input, [w_convs[0][0]], input_planes, channels, new_channels, noise_std, rand=rand, verify=verify)
+    w_wider, conv_next = conv_bn_wider(w_input, [w_convs[0][0]], input_planes,
+            channels, new_channels, noise_std, rand=rand, dir_alpha=dir_alpha, verify=verify)
     w_convs[0][0] = conv_next[0]
 
     write_layer(w_wider, out_file)
 
     for e, w in enumerate(w_convs[:-1]):
+        r = rand
         if e % 2 == 0:
             print("Processing block", 1 + e//2)
-        w_wider, conv_next = conv_bn_wider(w, [w_convs[e+1][0]], channels + new_channels, channels, new_channels, noise_std, rand=rand, verify=verify)
+            #First convolution in residual block can be widened randomly
+            r = None
+
+        w_wider, conv_next = conv_bn_wider(w, [w_convs[e+1][0]], channels + new_channels,
+                channels, new_channels, noise_std, rand=r, dir_alpha=dir_alpha, verify=verify)
         w_convs[e+1][0] = conv_next[0]
         write_layer(w_wider, out_file)
 
     #The last block is special case because of policy and value heads
-    w_wider, w_next = conv_bn_wider(w_convs[-1], [w_pol[0], w_val[0]], channels + new_channels, channels, new_channels, noise_std, last_block=True, rand=rand, verify=verify)
+    w_wider, w_next = conv_bn_wider(w_convs[-1], [w_pol[0], w_val[0]], channels + new_channels,
+            channels, new_channels, noise_std, last_block=True, rand=rand, dir_alpha=dir_alpha, verify=verify)
     w_pol[0] = w_next[0]
     w_val[0] = w_next[1]
 
