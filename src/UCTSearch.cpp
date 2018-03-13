47c0aeaa83074bacaea2ee8f715246563c9d0c8b
Andy Olsen <andy.olsen.tx@gmail.com>
2018-03-12 20:34:58 -0500

    Changes based on pull feedback.
    Add a few comments.
    Prevent overflow in summary output.

--- src/UCTSearch.cpp
+++ src/UCTSearch.cpp
@@ -593,11 +593,11 @@ int UCTSearch::think(int color, passflag_t passflag) {
     Time elapsed;
     int elapsed_centis = Time::timediff_centis(start, elapsed);
     if (elapsed_centis+1 > 0) {
-        myprintf("%d visits, %d nodes, %d playouts, %d n/s\n\n",
+        myprintf("%d visits, %d nodes, %d playouts, %.0f n/s\n\n",
                  m_root->get_visits(),
                  static_cast<int>(m_nodes),
                  static_cast<int>(m_playouts),
-                 (m_playouts * 100) / (elapsed_centis+1));
+                 (m_playouts * 100.0) / (elapsed_centis+1));
     }
     int bestmove = get_best_move(passflag);
 
@@ -641,6 +641,7 @@ void UCTSearch::set_playout_limit(int playouts) {
                                       decltype(m_maxplayouts)>::value,
                   "Inconsistent types for playout amount.");
     if (playouts == 0) {
+        // Divide max by 2 to prevent overflow corner cases.
         m_maxplayouts = std::numeric_limits<decltype(m_maxplayouts)>::max()/2;
     } else {
         m_maxplayouts = playouts;
@@ -652,6 +653,7 @@ void UCTSearch::set_visit_limit(int visits) {
                                       decltype(m_maxvisits)>::value,
                   "Inconsistent types for visits amount.");
     if (visits == 0) {
+        // Divide max by 2 to prevent overflow corner cases.
         m_maxvisits = std::numeric_limits<decltype(m_maxvisits)>::max()/2;
     } else {
         m_maxvisits = visits;
