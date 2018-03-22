767d58627c1e5ec116b38ad31f801a96d1228d22
michael <Michael O>
2018-03-23 06:58:15 +1100

    Fix net_to_model.py to work with tfprocess changes.

--- training/tf/net_to_model.py
+++ training/tf/net_to_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import tensorflow as tf
 import os
 import sys
 from tfprocess import TFProcess
@@ -23,14 +22,8 @@ with open(sys.argv[1], 'r') as f:
     blocks //= 8
     print("Blocks", blocks)
 
-x = [
-    tf.placeholder(tf.float32, [None, 18, 19 * 19]),
-    tf.placeholder(tf.float32, [None, 362]),
-    tf.placeholder(tf.float32, [None, 1])
-    ]
-
 tfprocess = TFProcess()
-tfprocess.init_net(x)
+tfprocess.init(batch_size=1)
 if tfprocess.RESIDUAL_BLOCKS != blocks:
     raise ValueError("Number of blocks in tensorflow model doesn't match "\
             "number of blocks in input network")
