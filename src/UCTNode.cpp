8a55a5073f50420cc9710b403aca6357bc7fd591
Jonathan Roy <jonroy7@gmail.com>
2018-03-22 18:24:49 -0400

    Use RANDOM_ROTATION again for the single evals

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -77,7 +77,7 @@ bool UCTNode::create_children(std::atomic<int>& nodecount,
     lock.unlock();
 
     const auto raw_netlist = Network::get_scored_moves(
-        &state, Network::Ensemble::MULTI, 1);
+        &state, Network::Ensemble::RANDOM_ROTATION);
 
     // DCNN returns winrate as side to move
     m_net_eval = raw_netlist.second;
