06f6bc94e3138f94d000c8c219f5b6a0eaf9963c
Junhee Yoo <ihavnoid@yahoo.com>
2018-03-27 00:54:24 -0500

    We also have to expand on find_child() because we may have cases where the child may not have been expanded yet

--- src/UCTNodeRoot.cpp
+++ src/UCTNodeRoot.cpp
@@ -146,7 +146,8 @@ UCTNode* UCTNode::get_nopass_child(FastState& state) const {
 UCTNode* UCTNode::find_child(const int move) {
     if (m_has_children) {
         for (auto& child : m_children) {
-            if (child->get_move() == move) {
+            if (child.get_move() == move) {
+                child.expand(); // no guarantee that this is a non-expanded node
                 return child.release();
             }
         }
