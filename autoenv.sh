49617b208c7e466147cd8bf22431e487c0c63e89
cngoodboy <cngoodboy@qq.com>
2018-04-18 13:29:00 +0800

    Create autoenv.sh

---
+++ autoenv.sh
@@ -0,0 +1,6 @@
+
+sudo apt install clinfo && clinfo
+
+git clone https://github.com/gcp/leela-zero
+cd leela-zero/src
+sudo apt install libboost-dev libboost-program-options-dev libopenblas-dev opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev zlib1g-dev
