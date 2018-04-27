8a87acedd984c9663edd00d8e757c225c278e3dd
TFiFiE <TFiFiE@users.noreply.github.com>
2018-04-27 13:38:28 +0200

    Suppress Clang warning in half.cpp.

--- src/half/half.hpp
+++ src/half/half.hpp
@@ -1078,7 +1078,7 @@ namespace half_float
 		friend struct detail::unary_specialized<half>;
 		friend struct detail::binary_specialized<half,half>;
 		template<typename,typename,std::float_round_style> friend struct detail::half_caster;
-		friend class std::numeric_limits<half>;
+		friend struct std::numeric_limits<half>;
 	#if HALF_ENABLE_CPP11_HASH
 		friend struct std::hash<half>;
 	#endif
