6fb405f581d8968bbdf80a175af169e02d1c60cf
Jonathan Roy <jonroy7@gmail.com>
2018-05-07 07:31:12 -0400

    Add 'average' option to heatmap gtp command.
    Pull request #1089.

--- src/Network.h
+++ src/Network.h
@@ -35,7 +35,7 @@
 class Network {
 public:
     enum Ensemble {
-        DIRECT, RANDOM_SYMMETRY
+        DIRECT, RANDOM_SYMMETRY, AVERAGE
     };
     using BoardPlane = std::bitset<BOARD_SQUARES>;
     using NNPlanes = std::vector<BoardPlane>;
