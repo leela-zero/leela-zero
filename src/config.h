cab4e924b00cb6e3108ef2641649348a5158c193
Gian-Carlo Pascutto <gcp@sjeng.org>
2018-05-08 18:12:26 +0200

    Fix BOARD_SQUARES macro affecting Dirichlet noise.
    This made the noise more uniform instead of peaky, which is not what we
    want.
    
    Fixes issue #1374.

--- src/config.h
+++ src/config.h
@@ -31,10 +31,11 @@
 #endif
 
 /*
- * BOARD_SIZE: Define size of the board to compile Leela with, must be an odd number due to winograd tiles
+ * BOARD_SIZE: Define size of the board to compile Leela with, must be an odd
+   number due to winograd tiles
  */
 #define BOARD_SIZE 19
-#define BOARD_SQUARES BOARD_SIZE*BOARD_SIZE
+#define BOARD_SQUARES (BOARD_SIZE*BOARD_SIZE)
 
 #if (BOARD_SIZE % 2 == 0)
 #error Code assumes odd board size, remove at your own risk!
