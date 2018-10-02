39355cbf395bb53fd27ad78718e68438ff2c661b
Junhee Yoo <ihavnoid@yahoo.com>
2018-10-03 08:14:06 +0900

    Network autodetection : don't initialize fp32 nets if we have fp16 compute

--- src/Network.h
+++ src/Network.h
@@ -124,7 +124,8 @@ private:
                                       const int symmetry);
     bool probe_cache(const GameState* const state, Network::Netresult& result);
     std::unique_ptr<ForwardPipe>&& init_net(int channels,
-                                            std::unique_ptr<ForwardPipe>&& pipe);
+                                            std::unique_ptr<ForwardPipe>&& pipe,
+                                            bool exit_if_no_need_autodetect = false);
 #ifdef USE_HALF
     void select_precision(int channels);
 #endif
