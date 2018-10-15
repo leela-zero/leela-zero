3680cbe3515bac28dba0af16a6cc5e03b7308991
Henrik Forstén <henrik.forsten@gmail.com>
2018-10-15 16:20:07 +0300

    Report tuner errors to stderr
    If tuner failed during precision autodetection error output in stdout was read
    as a GTP message.

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -82,21 +82,35 @@ bool Utils::input_pending() {
 
 static std::mutex IOmutex;
 
+static void myprintf_base(const char *fmt, va_list ap) {
+    va_list ap2;
+    va_copy(ap2, ap);
+
+    vfprintf(stderr, fmt, ap);
+
+    if (cfg_logfile_handle) {
+        std::lock_guard<std::mutex> lock(IOmutex);
+        vfprintf(cfg_logfile_handle, fmt, ap2);
+    }
+    va_end(ap2);
+}
+
 void Utils::myprintf(const char *fmt, ...) {
     if (cfg_quiet) {
         return;
     }
+
     va_list ap;
     va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
+    myprintf_base(fmt, ap);
     va_end(ap);
+}
 
-    if (cfg_logfile_handle) {
-        std::lock_guard<std::mutex> lock(IOmutex);
-        va_start(ap, fmt);
-        vfprintf(cfg_logfile_handle, fmt, ap);
-        va_end(ap);
-    }
+void Utils::myprintf_error(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    myprintf_base(fmt, ap);
+    va_end(ap);
 }
 
 static void gtp_fprintf(FILE* file, const std::string& prefix,
