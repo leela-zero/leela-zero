b83395256b50fb64537114fd1cf20437cf1f2e36
Junhee Yoo <33939814+ihavnoid@users.noreply.github.com>
2018-11-18 07:45:31 +0900

    CPU-only eval performance optimization.
    * CPUPipe : change winograd transformation constants to an equation.
    
    Combined with a series of strength reduction changes, 
    improves netbench by about 8%.
    
    * Convert some std::array into individual variables
    
    For some reason this allows gcc to optimize the code better,
    improving netbench by 2%.
    
    Pull request #2021.

--- src/CPUPipe.cpp
+++ src/CPUPipe.cpp
@@ -67,15 +67,34 @@ void CPUPipe::winograd_transform_in(const std::vector<float>& in,
     auto buffer_offset = 0;
     auto buffer_entries = 0;
 
-    std::array<std::array<float, WINOGRAD_ALPHA>, WINOGRAD_ALPHA> T1;
 
-    const auto Bt = std::array<float, WINOGRAD_TILE>
-               {1.0f,  0.0f,     -5.0f/2.0f,  0.0f,      1.0f, 0.0f,
-                0.0f, -SQ2,      -2.0f,       SQ2/2.0f,  1.0f, 0.0f,
-                0.0f,  SQ2,      -2.0f,      -SQ2/2.0f,  1.0f, 0.0f,
-                0.0f, -SQ2/2.0f, -1.0f/2.0f,  SQ2,       1.0f, 0.0f,
-                0.0f,  SQ2/2.0f, -1.0f/2.0f, -SQ2,       1.0f, 0.0f,
-                0.0f,  1.0f,      0.0f,      -5.0f/2.0f, 0.0f, 1.0f};
+    // multiple vector [i0..i5] by Bt and produce [o0..o5]
+    // const auto Bt = std::array<float, WINOGRAD_TILE>
+    //           {1.0f,  0.0f,     -5.0f/2.0f,  0.0f,      1.0f, 0.0f,
+    //            0.0f, -SQ2,      -2.0f,       SQ2/2.0f,  1.0f, 0.0f,
+    //            0.0f,  SQ2,      -2.0f,      -SQ2/2.0f,  1.0f, 0.0f,
+    //            0.0f, -SQ2/2.0f, -1.0f/2.0f,  SQ2,       1.0f, 0.0f,
+    //            0.0f,  SQ2/2.0f, -1.0f/2.0f, -SQ2,       1.0f, 0.0f,
+    //            0.0f,  1.0f,      0.0f,      -5.0f/2.0f, 0.0f, 1.0f};
+    auto multiply_bt = [](
+        float & o0, float & o1, float & o2, float & o3, float & o4, float & o5,
+        float i0, float i1, float i2, float i3, float i4, float i5
+    ) {
+        auto i3m1 = i1 * -SQ2 + i3 * (SQ2 / 2.0f);
+        auto i4m2 = i2 * -2.0f + i4 * 1.0f;
+
+        o0 = i0 + i2 * (-5.0f/2.0f) + i4;
+        o1 = i3m1 + i4m2;
+        o2 = -i3m1 + i4m2;
+
+        auto i3m1_2 = i3 * (SQ2) + i1 * (-SQ2/2.0f);
+        auto i4m2_2 = i2 * (-1.0f/2.0f) + i4;
+
+        o3 = i3m1_2 + i4m2_2;
+        o4 = -i3m1_2 + i4m2_2;
+
+        o5 = i1 + i3 * (-5.0f/2.0f) + i5;
+    };
 
     for (auto ch = 0; ch < C; ch++) {
         for (auto yin = 0; yin < H; yin++) {
@@ -88,28 +107,50 @@ void CPUPipe::winograd_transform_in(const std::vector<float>& in,
             const auto yin = WINOGRAD_M * block_y;
             for (auto block_x = 0; block_x < WTILES; block_x++) {
                 const auto xin = WINOGRAD_M * block_x;
+#define DECL_T1(XX) \
+                float T1_##XX##_0, T1_##XX##_1, T1_##XX##_2, T1_##XX##_3, T1_##XX##_4, T1_##XX##_5;
+                DECL_T1(0)
+                DECL_T1(1)
+                DECL_T1(2)
+                DECL_T1(3)
+                DECL_T1(4)
+                DECL_T1(5)
 
                 // Calculates transpose(B).x.B
-                for (auto i = 0; i < WINOGRAD_ALPHA; i++){
-                    for (auto j = 0; j < WINOGRAD_ALPHA; j++) {
-                        auto acc = 0.0f;
-                        for (auto k = 0; k < WINOGRAD_ALPHA; k++) {
-                            acc += Bt[i * WINOGRAD_ALPHA + k] * \
-                                   in_pad[yin + k][xin + j];
-                        }
-                        T1[i][j] = acc;
-                    }
-                }
+#define MULTIPLY_BT(XX) \
+                multiply_bt( \
+                    T1_0_##XX, T1_1_##XX, T1_2_##XX, T1_3_##XX, T1_4_##XX, T1_5_##XX, \
+                    in_pad[yin + 0][xin + XX], \
+                    in_pad[yin + 1][xin + XX], \
+                    in_pad[yin + 2][xin + XX], \
+                    in_pad[yin + 3][xin + XX], \
+                    in_pad[yin + 4][xin + XX], \
+                    in_pad[yin + 5][xin + XX] \
+                );
+                MULTIPLY_BT(0)
+                MULTIPLY_BT(1)
+                MULTIPLY_BT(2)
+                MULTIPLY_BT(3)
+                MULTIPLY_BT(4)
+                MULTIPLY_BT(5)
 
-                for (auto i = 0; i < WINOGRAD_ALPHA; i++){
-                    for (auto j = 0; j < WINOGRAD_ALPHA; j++) {
-                        auto acc = 0.0f;
-                        for (auto k = 0; k < WINOGRAD_ALPHA; k++) {
-                            acc += T1[i][k] * Bt[j * WINOGRAD_ALPHA + k];
-                        }
-                        buffer[buffersize * (i * WINOGRAD_ALPHA + j) + buffer_entries] = acc;
-                    }
-                }
+#define MULTIPLY_B(XX) \
+                multiply_bt( \
+                    buffer[buffersize * (XX * WINOGRAD_ALPHA + 0) + buffer_entries], \
+                    buffer[buffersize * (XX * WINOGRAD_ALPHA + 1) + buffer_entries], \
+                    buffer[buffersize * (XX * WINOGRAD_ALPHA + 2) + buffer_entries], \
+                    buffer[buffersize * (XX * WINOGRAD_ALPHA + 3) + buffer_entries], \
+                    buffer[buffersize * (XX * WINOGRAD_ALPHA + 4) + buffer_entries], \
+                    buffer[buffersize * (XX * WINOGRAD_ALPHA + 5) + buffer_entries], \
+                    T1_##XX##_0, T1_##XX##_1, T1_##XX##_2, T1_##XX##_3, T1_##XX##_4, T1_##XX##_5 \
+                );
+                MULTIPLY_B(0)
+                MULTIPLY_B(1)
+                MULTIPLY_B(2)
+                MULTIPLY_B(3)
+                MULTIPLY_B(4)
+                MULTIPLY_B(5)
+
                 if (buffer_entries == 0) {
                     buffer_offset = ch * P + block_y * WTILES + block_x;
                 }
@@ -165,6 +206,27 @@ void CPUPipe::winograd_transform_out(const std::vector<float>& M,
     constexpr auto WTILES = WINOGRAD_WTILES;
     constexpr auto P = WINOGRAD_P;
 
+    // multiple vector [i0..i5] by At and produce [o0..o3]
+    // const auto At = std::array<float, WINOGRAD_ALPHA * WINOGRAD_M>
+    //       {1.0f, 1.0f,      1.0f,       1.0f,      1.0f,     0.0f,
+    //        0.0f, SQ2/2.0f, -SQ2/2.0f,   SQ2,      -SQ2,      0.0f,
+    //        0.0f, 1.0f/2.0f, 1.0f/2.0f,  2.0f,      2.0f,     0.0f,
+    //        0.0f, SQ2/4.0f, -SQ2/4.0f,   2.0f*SQ2, -2.0f*SQ2, 1.0f};
+    auto multiply_at = [](
+        float & o0, float & o1, float & o2, float & o3,
+        float i0, float i1, float i2, float i3, float i4, float i5
+    ) {
+        auto t1p2 = (i1 + i2) * (1.0f / 2.0f);
+        auto t1m2 = (i1 - i2) * (SQ2/4.0f);
+        auto t3p4 = i3 + i4;
+        auto t3m4 = (i3 - i4) * (SQ2);
+
+        o0 = i0 + t1p2 + t1p2 + t3p4;
+        o1 = t1m2 + t1m2 + t3m4;
+        o2 = t1p2 + t3p4 + t3p4;
+        o3 = t1m2 + t3m4 + t3m4 + i5;
+    };
+
     for (auto k = 0; k < K; k++) {
         for (auto block_x = 0; block_x < WTILES; block_x++) {
             const auto x = WINOGRAD_M * block_x;
@@ -181,35 +243,22 @@ void CPUPipe::winograd_transform_out(const std::vector<float>& M,
                             M[(xi*WINOGRAD_ALPHA + nu)*K*P + k*P + b];
                     }
                 }
-
-                const auto At = std::array<float, WINOGRAD_ALPHA * WINOGRAD_M>
-                      {1.0f, 1.0f,      1.0f,       1.0f,      1.0f,     0.0f,
-                       0.0f, SQ2/2.0f, -SQ2/2.0f,   SQ2,      -SQ2,      0.0f,
-                       0.0f, 1.0f/2.0f, 1.0f/2.0f,  2.0f,      2.0f,     0.0f,
-                       0.0f, SQ2/4.0f, -SQ2/4.0f,   2.0f*SQ2, -2.0f*SQ2, 1.0f};
-
                 std::array<std::array<float, WINOGRAD_ALPHA>, WINOGRAD_M> temp;
                 std::array<std::array<float, WINOGRAD_M>, WINOGRAD_M> o;
 
                 // Calculates transpose(A).temp_m.A
-                for (auto i = 0; i < WINOGRAD_M; i++){
-                    for (auto j = 0; j < WINOGRAD_ALPHA; j++) {
-                        auto acc = 0.0f;
-                        for (auto q = 0; q < WINOGRAD_ALPHA; q++) {
-                            acc += At[i * WINOGRAD_ALPHA + q] * temp_m[q][j];
-                        }
-                        temp[i][j] = acc;
-                    }
+                for (auto j = 0; j < WINOGRAD_ALPHA; j++){
+                    multiply_at(
+                        temp[0][j], temp[1][j], temp[2][j], temp[3][j],
+                        temp_m[0][j], temp_m[1][j], temp_m[2][j], temp_m[3][j], temp_m[4][j], temp_m[5][j]
+                    );
                 }
 
                 for (auto i = 0; i < WINOGRAD_M; i++){
-                    for (auto j = 0; j < WINOGRAD_M; j++) {
-                        auto acc = 0.0f;
-                        for (auto q = 0; q < WINOGRAD_ALPHA; q++) {
-                            acc += temp[i][q] * At[j * WINOGRAD_ALPHA + q];
-                        }
-                        o[i][j] = acc;
-                    }
+                    multiply_at(
+                        o[i][0], o[i][1], o[i][2], o[i][3],
+                        temp[i][0], temp[i][1], temp[i][2], temp[i][3], temp[i][4], temp[i][5]
+                    );
                 }
 
                 const auto y_ind = k * H * W + y * W + x;
