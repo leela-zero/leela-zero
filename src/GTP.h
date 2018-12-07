39be654067127974c737769e68cf26f8c77ce40e
Henrik Forstén <henrik.forsten@gmail.com>
2018-12-07 14:18:44 +0200

    Add O(sqrt(log(n))) scaling to tree search.
    Pull request #2072.

--- src/GTP.h
+++ src/GTP.h
@@ -59,6 +59,8 @@ extern precision_t cfg_precision;
 #endif
 #endif
 extern float cfg_puct;
+extern float cfg_logpuct;
+extern float cfg_logconst;
 extern float cfg_softmax_temp;
 extern float cfg_fpu_reduction;
 extern float cfg_fpu_root_reduction;
