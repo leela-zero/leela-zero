76433cb04be061bb520085d670b25d81ce729a5a
Henrik Forstén <henrik.forsten@gmail.com>
2018-12-19 16:48:57 +0200

    Add double

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -251,7 +251,8 @@ UCTNode* UCTNode::uct_select_child(int color, bool is_root) {
         }
     }
 
-    const auto numerator = std::sqrt(double(parentvisits) * std::log(cfg_logpuct * parentvisits + cfg_logconst));
+    const auto numerator = std::sqrt(double(parentvisits) *
+            std::log(cfg_logpuct * double(parentvisits) + cfg_logconst));
     const auto fpu_reduction = (is_root ? cfg_fpu_root_reduction : cfg_fpu_reduction) * std::sqrt(total_visited_policy);
     // Estimated eval for unknown nodes = original parent NN eval - reduction
     const auto fpu_eval = get_net_eval(color) - fpu_reduction;
