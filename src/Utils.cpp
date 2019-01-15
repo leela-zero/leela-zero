afb59466c3190d80c4017a5c520996bd694bc498
Alexander Taylor <alexanderjohntaylor@gmail.com>
2019-01-15 10:41:50 +0000

    Fixed leelaz_file on Android.
    Pull request #2135.

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -188,7 +188,7 @@ size_t Utils::ceilMultiple(size_t a, size_t b) {
 }
 
 const std::string Utils::leelaz_file(std::string file) {
-#ifdef _WIN32
+#if defined(_WIN32) || defined(__ANDROID__)
     boost::filesystem::path dir(boost::filesystem::current_path());
 #else
     // https://stackoverflow.com/a/26696759
