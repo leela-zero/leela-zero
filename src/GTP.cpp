efdb1580cc68fa97a41f6004485ea6ca64ac2f68
Gian-Carlo Pascutto <gcp@sjeng.org>
2019-02-04 12:31:44 +0100

    Add lz-analyze minmoves tag.
    Add an lz-analyze tag to suggest the minimum amount of moves the
    engine should post info about (rather than only those it considers
    interesting, i.e. the ones with at least a visit).
    
    This allows some very flexible constructs:
    
    Getting a heatmap:
    
        lz-setoption name visits value 1
        lz-analyze interval 1 minmoves 361
    
    Forcing a move among the top policy moves only:
    
        lz-setoption name visits value 1
        lz-analyze interval 1 minmoves 2
        (store those moves, e.g. A1, B1)
        lz-setoption name visits value 0
        lz-genmove_analyze b interval 1 allow b A1 1 allow b B1 1

--- src/GTP.cpp
+++ src/GTP.cpp
@@ -230,6 +230,11 @@ AnalyzeTags::AnalyzeTags(std::istringstream& cmdstream, const GameState& game) {
             if (cmdstream.fail()) {
                 return;
             }
+        } else if (tag == "minmoves") {
+            cmdstream >> m_min_moves;
+            if (cmdstream.fail()) {
+                return;
+            }
         } else {
             return;
         }
@@ -254,6 +259,10 @@ int AnalyzeTags::invalid() const {
 
 int AnalyzeTags::who() const {
     return m_who;
+}
+
+size_t AnalyzeTags::post_move_count() const {
+    return m_min_moves;
 }
 
 bool AnalyzeTags::is_to_avoid(int color, int vertex, size_t movenum) const {
