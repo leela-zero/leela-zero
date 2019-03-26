1f4582b4e44e281b067a634d0319e6560d661f0f
Henrik Forstén <henrik.forsten@gmail.com>
2019-03-26 10:57:46 +0200

    Choose move based on normal distribution LCB.
    * Calculate node variance.
    * Use normal distribution LCB to choose the played move.
    * Cached student-t.
    * Sort lz-analyze output according to LCB.
    * Don't choose nodes with very few visits even if LCB is better.
    
    Guard against NN misevaluations when top move has lot of visits.
    Without this it's possible for move with few hundred visits to be picked
    over a move with over ten thousand visits.
    
    The problem is that the evaluation distribution isn't really normal
    distribution. Evaluations correlate and the distribution can change
    if deeper in the tree it finds a better alternative.
    
    Pull request #2290.

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -35,6 +35,7 @@
 #include <cstdio>
 
 #include <boost/filesystem.hpp>
+#include <boost/math/distributions/students_t.hpp>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -48,6 +49,30 @@
 #include "GTP.h"
 
 Utils::ThreadPool thread_pool;
+
+auto constexpr z_entries = 1000;
+std::array<float, z_entries> z_lookup;
+
+void Utils::create_z_table() {
+    for (auto i = 1; i < z_entries + 1; i++) {
+        boost::math::students_t dist(i);
+        auto z = boost::math::quantile(boost::math::complement(dist, cfg_ci_alpha));
+        z_lookup[i - 1] = z;
+    }
+}
+
+float Utils::cached_t_quantile(int v) {
+    if (v < 1) {
+        return z_lookup[0];
+    }
+    if (v < z_entries) {
+        return z_lookup[v - 1];
+    }
+    // z approaches constant when v is high enough.
+    // With default lookup table size the function is flat enough that we
+    // can just return the last entry for all v bigger than it.
+    return z_lookup[z_entries - 1];
+}
 
 bool Utils::input_pending() {
 #ifdef HAVE_SELECT
