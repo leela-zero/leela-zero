509ed0bdf548a464391ffca83c84a1017a2793c5
Michael Reilly <OmnipotentEntity@gmail.com>
2019-03-28 19:13:35 -0400

    Clean up syntax and usage to be normal (thanks #nixos) and add dependencies for training

--- shell.nix
+++ shell.nix
@@ -4,27 +4,33 @@ let
 
 in stdenv.mkDerivation {
   name = "env";
+  nativeBuildInputs = [
+    pkgs.cmake
+  ];
+
   buildInputs = [
-    pkgs.zlib.dev
-    pkgs.boost.dev
+    pkgs.zlib
+    pkgs.boost
     pkgs.qt5.full
     pkgs.opencl-headers
     pkgs.ocl-icd
+    pkgs.python3
+    pkgs.python37Packages.tensorflow
+    pkgs.python37Packages.pymongo
+    pkgs.python37Packages.numpy
   ];
 
-  shellHook = ''
-    export PKG_CONFIG_PATH=${pkgs.zlib}/lib/pkgconfig
-    export BOOST_ROOT=${pkgs.boost.dev}
-    export BOOST_INCLUDEDIR=${pkgs.boost.dev}/include
-    export BOOST_LIBRARYDIR=${pkgs.boost}/lib
-    export LIBRARY_PATH=${pkgs.ocl-icd}/lib:${pkgs.boost}/lib:${pkgs.zlib}/lib
-    mkdir -p build/
-    cd build
-    cmake -DCMAKE_BUILD_TYPE=Release \
-      -DZLIB_INCLUDE_DIR=${pkgs.zlib.dev}/include \
-      -DZLIB_LIBRARY=${pkgs.zlib}/lib/libz.so \
-      -DOpenCL_LIBRARY=${pkgs.ocl-icd}/lib/libOpenCL.so \
-      -DOpenCL_INCLUDE_DIR=${pkgs.opencl-headers}/include \
-      .. 1>&2
-  '';
+  cmakeFlags = with stdenv; [
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DZLIB_INCLUDE_DIR=${pkgs.zlib.dev}/include"
+    "-DZLIB_LIBRARY=${pkgs.zlib}/lib/libz.so"
+    "-DOpenCL_LIBRARY=${pkgs.ocl-icd}/lib/libOpenCL.so"
+    "-DOpenCL_INCLUDE_DIR=${pkgs.opencl-headers}/include"
+  ];
+
+  PKG_CONFIG_PATH = "${pkgs.zlib}/lib/pkgconfig";
+  BOOST_ROOT = "${pkgs.boost.dev}";
+  BOOST_INCLUDEDIR = "${pkgs.boost.dev}/include";
+  BOOST_LIBRARYDIR = "${pkgs.boost}/lib";
+  LIBRARY_PATH = "${pkgs.ocl-icd}/lib:${pkgs.boost}/lib:${pkgs.zlib}/lib";
 }
