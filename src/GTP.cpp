86d5488ee9d04075b14c56745d6cf1d286f11754
Jonathan Roy <jonroy7@gmail.com>
2019-04-14 16:38:52 -0400

    cfg_puct was being set twice

--- src/GTP.cpp
+++ src/GTP.cpp
@@ -341,7 +341,6 @@ void GTP::setup_default_parameters() {
     cfg_precision = precision_t::AUTO;
 #endif
 #endif
-    cfg_puct = 0.5f;
     cfg_logpuct = 0.015f;
     cfg_logconst = 1.7f;
     cfg_softmax_temp = 1.0f;
