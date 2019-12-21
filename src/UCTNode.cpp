d011c7208dfe8c72ac9f5666783dafb5e9db57f3
TFiFiE <TFiFiE@users.noreply.github.com>
2019-12-21 15:53:38 +0100

    Fixed commentary.

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -315,9 +315,9 @@ UCTNode* UCTNode::uct_select_child(int color, bool is_root) {
     const auto numerator = std::sqrt(double(parentvisits) *
             std::log(cfg_logpuct * double(parentvisits) + cfg_logconst));
     const auto fpu_reduction = (is_root ? cfg_fpu_root_reduction : cfg_fpu_reduction) * std::sqrt(total_visited_policy);
-    // Estimated eval for unknown nodes = original parent NN eval - reduction
+    // Estimated eval for unknown nodes = parent (not NN) eval - reduction
     const auto fpu_eval = get_raw_eval(color) - fpu_reduction;
-    // use eval of parent instead of net_eval
+
     auto best = static_cast<UCTNodePointer*>(nullptr);
     auto best_value = std::numeric_limits<double>::lowest();
 
